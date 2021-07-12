4a325d2ab0475c8ebc6bb0d9bb95cc5eae878a50
Lope Emano <lopeemano@gmail.com>
2021-07-12 14:50:07 +0800

    add commands

---
+++ lope-first/intro.md
@@ -0,0 +1,6 @@
+install ping
+`apt update`{{execute}}
+`apt install -y inetutils-ping`{{execute}}
+
+install curl
+`apt install -y curl`{{execute}}
--- lope-first/set-env.sh
+++ lope-first/set-env.sh
@@ -1,4 +1,5 @@
 launch.sh
+
 export SCENARIO_VERSION=1.1
 kubectl scale --replicas=0 Deployment/coredns -n kube-system >> /root/.logs
 kubectl apply -f /root/deploy.yaml

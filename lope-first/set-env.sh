04337393d364422298f5af0ec05d960b766bf6f2
Lope Emano <lopeemano@gmail.com>
2021-07-12 15:01:05 +0800

    add more descriptions

--- lope-first/set-env.sh
+++ lope-first/set-env.sh
@@ -1,7 +1,7 @@
 launch.sh
 
 export SCENARIO_VERSION=1.1
-kubectl scale --replicas=0 Deployment/coredns -n kube-system >> /root/.logs
+kubectl scale --replicas=0 Deployment/coredns -n kube-system >> /dev/null
 kubectl apply -f /root/deploy.yaml
 kubectl apply -f /root/nginx.yaml
 kubectl apply -f /root/svc.yaml

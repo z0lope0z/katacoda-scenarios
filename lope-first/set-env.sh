d523989a9446c7c9e88a34578da5861c64566f3c
Lope Emano <lopeemano@gmail.com>
2021-07-12 14:32:40 +0800

    upload yaml files for deploy, nginx, and svc

--- lope-first/set-env.sh
+++ lope-first/set-env.sh
@@ -1,2 +1,5 @@
 launch.sh
 kubectl scale --replicas=0 Deployment/coredns -n kube-system >> /root/.logs
+kubectl apply -f /root/deploy.yaml
+kubectl apply -f /root/nginx.yaml
+kubectl apply -f /root/svc.yaml
